d5b9bc180b1ba6a678dd2cb1f28b2eae9a6c6f48
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-08-17 15:01:12 -0400

    Make the homepage responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,26 +14,26 @@
   <h1 class="text-3xl font-bold">Select a Quiz</h1>
 
   <h2 class="mb-2 mt-6 text-2xl font-bold">By Unit</h2>
-  <section class="grid grid-cols-4 gap-4">
+  <section class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
     {#each units as unit}
       <QuizCard name={collections[unit].displayName} slug={unit} />
     {/each}
   </section>
 
   <h2 class="mb-2 mt-6 text-2xl font-bold">By Textbook Chapter</h2>
-  <section class="grid grid-cols-4 gap-4">
+  <section class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
     {#each chapters as chapter}
       <QuizCard name="Chapter {chapter}" slug="apes_ch{chapter}" />
     {/each}
   </section>
 
   <h2 class="mb-2 mt-6 text-2xl font-bold">Review the Course</h2>
-  <section class="grid grid-cols-4 gap-4">
+  <section class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
     <QuizCard name="Review Quiz 1" slug="apes_rev1" />
     <QuizCard name="Review Quiz 2" slug="apes_rev2" />
     <QuizCard name="Review Quiz 3" slug="apes_rev3" />
     <QuizCard name="2022 Final Review" slug="apes_f22" />
-    <QuizCard class="col-span-2" name="Complete APES Review" slug="complete_review" />
-    <QuizCard class="col-span-2" name="Everything" slug="everything" />
+    <QuizCard class="sm:col-span-2" name="Complete APES Review" slug="complete_review" />
+    <QuizCard class="sm:col-span-2" name="Everything" slug="everything" />
   </section>
 </main>
