ddc7e66b52df159b3631ce3b26b3eb96a0f38eed
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-12-13 22:58:54 -0500

    Fix Cloudflare analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
   import "@fontsource-variable/outfit";
   import "../app.css";
-  import Analytics from "../components/Analytics.svelte";
   import Footer from "../components/Footer.svelte";
 </script>
 
 <slot />
 
 <Footer />
-<Analytics />
