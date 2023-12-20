a90c5fd9139056a9d60552f2a79dfcbae34a7814
doraemon-daffodils <satyabrata@nujs.edu>
2023-12-20 11:51:50 +0530

    dev mode

--- generate-sitemap.mjs
+++ generate-sitemap.mjs
@@ -1,5 +1,5 @@
 import { createSitemap } from "svelte-sitemap/src/index.js";
 
-createSitemap(process.env.BASE_URL ?? "https://webquiz.bswanson.dev/", {
+createSitemap(process.env.BASE_URL ?? "https://civils.app/", {
   outDir: process.env.VERCEL ? ".vercel/output/static" : undefined,
 });
--- src/routes/quiz/[quiz]/+page.svelte
+++ src/routes/quiz/[quiz]/+page.svelte
@@ -115,14 +115,14 @@
   <h1 class="text-3xl font-bold">
     {quizName}
   </h1>
-  <div class="hidden break-before-avoid opacity-75 print:block">
+  <!-- <div class="hidden break-before-avoid opacity-75 print:block">
     <p>
       WebQuiz: a project by <a href="https://bswanson.dev"
         ><img src="https://bswanson.dev/favicon.svg" width={16} height={16} alt="" class="inline" />
         Brendan Swanson</a>
     </p>
     <p class="underline">https://webquiz.bswanson.dev/</p>
-  </div>
+  </div> -->
   <p class="mb-8">{questions.length} questions</p>
 
   {#if recoveredAnswers}
