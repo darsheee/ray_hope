eb4e796e9261b9eb74198fcfa8116988dc9dfdc2
darsheee <satyabrata@nujs.edu>
2024-03-04 10:22:42 +0530

    header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,17 @@
   ) as (keyof typeof collections)[];
 
   const chapters = Array.from({ length: 24 }).map((_, i) => i + 1);
+
+ const crashs = Array.from({ length: 3 }).map(
+    (_, i) => `crash_${i + 1}`
+  ) as (keyof typeof collections)[];
+
+ 
+
 </script>
 
 <svelte:head>
-  <title>upsc.study</title>
+  <title>Ias.Shiksha</title>
   <meta name="og:title" content="upsc.study" />
   
 </svelte:head>
@@ -19,7 +26,7 @@
 <main class="mx-auto max-w-3xl">
   <!-- <h1 class="text-3xl font-bold">Civils.app</h1> -->
 
-  <h2 class="mb-2 mt-6 text-xl font-bold">Previous Year Questions (1995 - 2023)</h2>
+  <h2 class="mb-2 mt-6 text-xl font-bold">Previous Year Questions </h2>
   <section class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
    
     <QuizCard name="Ancient History " slug="ancient_history_pyq" />
@@ -33,12 +40,9 @@
     <QuizCard name="Science" slug="science_pyq" />
     <QuizCard name="Current Affairs " slug="pyq_current-affairs" />
 
-    <QuizCard name="Review Quiz 1" slug="apes_rev1" />
-    <QuizCard name="Review Quiz 2" slug="apes_rev2" />
-    <QuizCard name="Review Quiz 3" slug="apes_rev3" />
-    <QuizCard name="2022 Final Review" slug="apes_f22" />
-    <QuizCard class="sm:col-span-2" name="Complete APES Review" slug="complete_review" />
-    <QuizCard class="sm:col-span-2" name="Everything" slug="everything" />
+    
+    <!-- <QuizCard class="sm:col-span-2" name="Complete APES Review" slug="complete_review" />
+    <QuizCard class="sm:col-span-2" name="Everything" slug="everything" /> -->
   </section>
 
   <h2 class="mb-2 mt-6 text-xl font-bold">By Unit</h2>
@@ -48,12 +52,20 @@
     {/each}
   </section>
 
-  <h2 class="mb-2 mt-6 text-xl font-bold">By Textbook Chapter</h2>
+   <h2 class="mb-2 mt-6 text-xl font-bold"> Polity Questions</h2>
+  <section class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+    {#each crashs as crash}
+      <QuizCard name={collections[crash].displayName} slug={crash} />
+    {/each}
+  </section>
+
+
+  <!-- <h2 class="mb-2 mt-6 text-xl font-bold">By Textbook Chapter</h2>
   <section class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
     {#each chapters as chapter}
       <QuizCard name="Chapter {chapter}" slug="apes_ch{chapter}" />
     {/each}
-  </section>
+  </section> -->
 
   
 </main>
